e978fa2c0dd42549f46ee0ee5c5078664b8d7bbd
Kline Borromeo <kline.borromeo2@mail.dcu.ie>
2021-10-06 13:34:59 +0100

    deleted shit

--- week-02/echo-arguments.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# http://faculty.salina.k-state.edu/tim/unix_sg/shell/variables.html#:~:text=Simply%20list%20the%20arguments%20on,third%20argument%2C%20etc...
-
-for ARG in "$@" ## One per line
-do
-    echo $ARG
-done
--- week-02/first-non-file.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-while read FNAME && test -f "$FNAME"
-do
-    : ## : is equivalent to the True command
-done
-echo "$FNAME"
